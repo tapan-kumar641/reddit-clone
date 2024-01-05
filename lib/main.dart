d34d01322bbeeb6ef7c94c163a772240a4416828
tapan kumar <tapan.kumar@pixelunited.com>
2024-01-06 01:20:33 +0530

    Hello from windows

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Scaffold(body: Text('Hello 3')),
+      home: const Scaffold(body: Text('Hello from windows')),
     );
   }
 }
